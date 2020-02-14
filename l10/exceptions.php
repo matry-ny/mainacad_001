464b67ef97ae27f5dbe600373ee088d7c5ea0743
Dmytro Kotenko <dmytro.kotenko@mgid.com>
2020-02-14 19:29:28 +0200

    L10: Git exceptions demonstration

--- l10/exceptions.php
+++ l10/exceptions.php
@@ -1,7 +1,30 @@
 <?php
 
+class MyOwnException extends Exception
+{
+    protected $message = 'My message';
+
+    protected $code = 321;
+}
+
 try {
-    throw new RuntimeException('Test');
+    $int = random_int(1, 2);
+    var_dump($int);
+    if ($int % 2 === 0) {
+//        throw new RuntimeException('Test');
+//        throw new ErrorException('Error');
+        throw new LogicException('Logic');
+    } else {
+        throw new MyOwnException();
+    }
 } catch (RuntimeException $exception) {
-    var_dump($exception);
+    var_dump('RUNTIME', $exception);
+} catch (ErrorException $exception) {
+    var_dump('ERROR', $exception);
+} catch (MyOwnException $exception) {
+    var_dump('My OWN', $exception);
+} catch (Exception $exception) {
+    var_dump('OTHER', $exception);
+} finally {
+    var_dump('AFTER EXCEPTION');
 }
